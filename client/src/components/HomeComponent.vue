658d4bcbb320d98c65e408676a7022381168ff9d
nejat-njonjo <nejatmhango@gmail.com>
2021-01-08 11:11:24 +0200

    added list

--- client/src/components/HomeComponent.vue
+++ client/src/components/HomeComponent.vue
@@ -1,6 +1,38 @@
 <template>
   <div class="home-page">
     <BackupForm />
+    <v-list
+      subheader
+      two-line
+    >
+      <v-subheader inset>Databases</v-subheader>
+
+      <v-list-item
+        v-for="folder in folders"
+        :key="folder.title"
+      >
+        <v-list-item-avatar>
+          <v-icon
+            class="grey lighten-1"
+            dark
+          >
+            mdi-database
+          </v-icon>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title v-text="folder.title"></v-list-item-title>
+
+          <v-list-item-subtitle v-text="folder.subtitle"></v-list-item-subtitle>
+        </v-list-item-content>
+
+        <v-list-item-action>
+          <v-btn icon title="Backup">
+            <v-icon color="grey lighten-1">mdi-backup-restore</v-icon>
+          </v-btn>
+        </v-list-item-action>
+      </v-list-item>
+    </v-list>
   </div>
 </template>
 
@@ -11,7 +43,38 @@ export default {
   name: 'HomeComponent',
   components: {
     BackupForm
-  }
+  },
+
+  data: () => ({
+    files: [
+      {
+        color: 'blue',
+        icon: 'mdi-clipboard-text',
+        subtitle: 'Jan 20, 2014',
+        title: 'Vacation itinerary',
+      },
+      {
+        color: 'amber',
+        icon: 'mdi-gesture-tap-button',
+        subtitle: 'Jan 10, 2014',
+        title: 'Kitchen remodel',
+      },
+    ],
+    folders: [
+      {
+        subtitle: 'Jan 9, 2014',
+        title: 'Photos',
+      },
+      {
+        subtitle: 'Jan 17, 2014',
+        title: 'Recipes',
+      },
+      {
+        subtitle: 'Jan 28, 2014',
+        title: 'Work',
+      },
+    ],
+  })
 }
 </script>
 
