f6212684bb227c8834a96d259db4a7712affb000
nejat-njonjo <nejatmhango@gmail.com>
2021-01-08 12:08:00 +0200

    added database route

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,11 +1,14 @@
 const router = require('express').Router()
 const backupController = require('../controllers/backup-controller')
+const databaseController = require('../controllers/database-controller')
 
 /**
  * Backup Routes
  */
 router.get('/backups', backupController.loadBuckups)
       .post('/backup', backupController.backup)
+
+router.get('/databases', databaseController.listDatabases)
 
 
 module.exports = router
