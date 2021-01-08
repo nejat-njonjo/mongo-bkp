1136179626eefd27f63068857a2fb33d753966ca
nejat-njonjo <nejatmhango@gmail.com>
2021-01-08 12:07:47 +0200

    added config

--- server/config/index.js
+++ server/config/index.js
@@ -1,21 +1,23 @@
 const routes = require('./routes');
 const port = () => process.env.PORT || '3000'
 
-module.exports = {
-  routes,
-  port: port(),
-  API: {
-    baseUrl: '/api/v1'
-  },
-  Mongo: {
-    username: '',
-    password: '',
-    hostname: 'localhost',
-    port: '27017',
-    dbname: 'my-backups'
-  },
-  Events: {
-    DB_CONN_ERR: 'DB_CONN_ERR',
-    DB_CONN_OK: 'DB_CONN_OK'
+module.exports = Object.freeze(
+  {
+    routes,
+    port: port(),
+    API: {
+      baseUrl: '/api/v1'
+    },
+    Mongo: {
+      username: '',
+      password: '',
+      hostname: 'localhost',
+      port: '27017',
+      dbname: 'my-backups'
+    },
+    Events: {
+      DB_CONN_ERR: 'DB_CONN_ERR',
+      DB_CONN_OK: 'DB_CONN_OK'
+    }
   }
-}
+)
