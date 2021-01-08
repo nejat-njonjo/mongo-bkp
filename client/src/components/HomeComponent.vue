40d00b36fed203449ee7322928f616855a1edd05
nejat-njonjo <nejatmhango@gmail.com>
2021-01-08 12:07:13 +0200

    added database explore

--- client/src/components/HomeComponent.vue
+++ client/src/components/HomeComponent.vue
@@ -8,8 +8,8 @@
       <v-subheader inset>Databases</v-subheader>
 
       <v-list-item
-        v-for="folder in folders"
-        :key="folder.title"
+        v-for="database in databases"
+        :key="database.name"
       >
         <v-list-item-avatar>
           <v-icon
@@ -21,15 +21,17 @@
         </v-list-item-avatar>
 
         <v-list-item-content>
-          <v-list-item-title v-text="folder.title"></v-list-item-title>
-
-          <v-list-item-subtitle v-text="folder.subtitle"></v-list-item-subtitle>
+          <v-list-item-title v-text="database.name"></v-list-item-title>
+          <v-list-item-subtitle v-text="'Size: ' + formatBytes(database.sizeOnDisk)"></v-list-item-subtitle>
+          <v-list-item-subtitle v-text="database.empty ? 'Empty: YES': 'Empty: NO'"></v-list-item-subtitle>
         </v-list-item-content>
 
         <v-list-item-action>
-          <v-btn icon title="Backup">
+          <v-btn icon title="Backup" @click="toggleBackup(database.name)">
             <v-icon color="grey lighten-1">mdi-backup-restore</v-icon>
           </v-btn>
+
+          <BackupForm :database="database.name" v-if="activeDatabase === database.name"/>
         </v-list-item-action>
       </v-list-item>
     </v-list>
@@ -38,6 +40,7 @@
 
 <script>
 import BackupForm from './BackupForm'
+import DBService from '../services/db-service'
 
 export default {
   name: 'HomeComponent',
@@ -46,35 +49,36 @@ export default {
   },
 
   data: () => ({
-    files: [
-      {
-        color: 'blue',
-        icon: 'mdi-clipboard-text',
-        subtitle: 'Jan 20, 2014',
-        title: 'Vacation itinerary',
-      },
-      {
-        color: 'amber',
-        icon: 'mdi-gesture-tap-button',
-        subtitle: 'Jan 10, 2014',
-        title: 'Kitchen remodel',
-      },
-    ],
-    folders: [
-      {
-        subtitle: 'Jan 9, 2014',
-        title: 'Photos',
-      },
-      {
-        subtitle: 'Jan 17, 2014',
-        title: 'Recipes',
-      },
-      {
-        subtitle: 'Jan 28, 2014',
-        title: 'Work',
-      },
-    ],
-  })
+    databases: [],
+    activeDatabase: null
+  }),
+
+  async mounted() {
+    this.databases = await this.loadDatabases()
+  },
+
+  methods: {
+    async loadDatabases() {
+      const response = await DBService.getDatabases()
+      return response.databases
+    },
+
+    formatBytes(bytes, decimals = 2) {
+      if (bytes === 0) return '0 Bytes';
+
+      const k = 1024;
+      const dm = decimals < 0 ? 0 : decimals;
+      const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+      const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+      return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+    },
+
+    toggleBackup(dbname) {
+      this.activeDatabase = this.activeDatabase !== dbname ? dbname : null
+    }
+  }
 }
 </script>
 
