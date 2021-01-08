b7277040df85b07c0655a44197ada4a6c1d6db5b
nejat-njonjo <nejatmhango@gmail.com>
2021-01-08 11:15:59 +0200

    added build target directory

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,18 @@
+// disable eslint
+
+const path = require('path')
+
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  outputDir: path.resolve(__dirname, '../public'),
+  devServer: {
+    proxy: {
+      '/api/v1': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    }
+  }
 }
