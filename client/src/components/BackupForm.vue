3d8730394e0ff1a88d8daca918242fa8e4dbaf4e
nejat-njonjo <nejatmhango@gmail.com>
2021-01-08 10:33:26 +0200

    created a backup form component

---
+++ client/src/components/BackupForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="backup-form">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BackupForm',
+
+  data: () => ({
+    backupForm: {
+      host: null,
+      port: null,
+      database: null,
+      directory: null
+    }
+  }),
+
+  methods: {
+    createBackup() {
+      console.log(this.backupForm)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
