a93acf405ef652c62fdef757ffae23727f1aa8f9
nejat-njonjo <nejatmhango@gmail.com>
2021-01-11 15:08:37 +0200

    co

--- server/services/backup-service.js
+++ server/services/backup-service.js
@@ -2,7 +2,7 @@ const shell = require('shelljs')
 const config = require('./config')
 
 const dumpDatabase = (database, dir) => {
-  const dump = shell.exec(`mongodump --username=${config.localUsername} --password=${config.localPassword} --db=${database} --out=${dir}`)
+  const dump = shell.exec(`mongodump --username=${config.localUsername} --authenticationDatabase admin --password=${config.localPassword} --db=${database} --out=${dir}`)
 
   if (dump.code !== 0) {
     return false
