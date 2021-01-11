50fdd7258e337f01beb7f199fe9d2cb3448360e6
nejat-njonjo <nejatmhango@gmail.com>
2021-01-11 15:06:19 +0200

    added local db config

--- server/services/config.js
+++ server/services/config.js
@@ -3,6 +3,8 @@ const path = require('path')
 module.exports = {
   outDir: path.resolve(`${__dirname}../../backup`),
   database: 'db-hcs',
+  localUsername: 'admin',
+  localPassword: 'admin',
   atlasUsername: 'dataframe',
   atlasPassword: 'Mlambe101',
   atlasProtocol: 'mongodb+srv',
